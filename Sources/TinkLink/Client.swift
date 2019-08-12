f4759c88ead4e03275d1af64c06948a89b20b4b0
Kasper Lahti <kasper.lahti@tink.se>
2019-08-12 16:35:09 +0200

    Add client initializer setting up channel

--- Sources/TinkLink/Client.swift
+++ Sources/TinkLink/Client.swift
@@ -1,4 +1,23 @@
 import Foundation
+import SwiftGRPC
 
 public final class Client {
+    let channel: Channel
+
+    public init(environment: Environment, clientKey: String, userAgent: String? = nil, certificateURL: URL? = nil) {
+        var arguments: [Channel.Argument] = []
+
+        arguments.append(.maxReceiveMessageLength(20 * 1024 * 1024))
+
+        if let userAgent = userAgent {
+            arguments.append(.primaryUserAgent(userAgent))
+        }
+
+        if let certificateURL = certificateURL {
+            let certificateContents = try! String(contentsOf: certificateURL, encoding: .utf8)
+            self.channel = Channel(address: environment.url.absoluteString, certificates: certificateContents, clientCertificates: nil, clientKey: clientKey, arguments: arguments)
+        } else {
+            self.channel = Channel(address: environment.url.absoluteString, secure: false, arguments: arguments)
+        }
+    }
 }
