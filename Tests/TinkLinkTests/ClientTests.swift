04eb3bbcb5179bedb52cba2d9cf9b53d1c8409cd
Kasper Lahti <kasper.lahti@tink.se>
2019-08-13 09:29:29 +0200

    Add test for unauthenticated client call

--- Tests/TinkLinkTests/ClientTests.swift
+++ Tests/TinkLinkTests/ClientTests.swift
@@ -1,7 +1,36 @@
 import XCTest
 @testable import TinkLink
+import SwiftGRPC
 
 class ClientTests: XCTestCase {
-    func testClient() {
+    func testUnauthenticatedClient() {
+        let client = Client(
+            environment: .staging,
+            clientKey: ProcessInfo.processInfo.environment["TINK_CLIENT_KEY"]!,
+            certificateURL: Bundle(for: Client.self).url(forResource: "staging", withExtension: "pem")!
+        )
+
+        let requestExpectation = expectation(description: "Providers Request")
+
+        client.providerService.providers { (result) in
+            do {
+                _ = try result.get()
+                XCTFail("Shouldn't receive providers when not authenticated.")
+            } catch let rpcError as RPCError {
+                switch rpcError {
+                case .invalidMessageReceived:
+                    XCTFail("Invalid message received")
+                case .timedOut:
+                    XCTFail("Request timed out")
+                case .callError(let callResult):
+                    XCTAssertEqual(callResult.statusCode, .unauthenticated)
+                }
+            } catch {
+                XCTFail(error.localizedDescription)
+            }
+            requestExpectation.fulfill()
+        }
+
+        wait(for: [requestExpectation], timeout: 5)
     }
 }
