a4c60c7d3a21bb25389e5620cc104c5400770aaa
Kasper Lahti <kasper.lahti@tink.se>
2019-08-13 11:50:20 +0200

    Add TinkLink target and product for SPM

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:5.0
+
+import PackageDescription
+
+let package = Package(
+    name: "TinkLink",
+    products: [
+        .library(
+            name: "TinkLink",
+            targets: ["TinkLink"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-protobuf.git", .upToNextMajor(from: "1.5.0")),
+        .package(url: "https://github.com/grpc/grpc-swift.git", .upToNextMajor(from: "0.9.1"))
+    ],
+    targets: [
+        .target(
+            name: "TinkLink",
+            dependencies: ["TinkGRPC"]),
+        .testTarget(
+            name: "TinkLinkTests",
+            dependencies: ["TinkLink"]),
+        .target(
+            name: "TinkGRPC",
+            dependencies: ["SwiftProtobuf", "SwiftGRPC"]),
+    ]
+)
