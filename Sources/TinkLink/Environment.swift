bc42e2aca7afb5912a3bd7b683bf8a33f332a7bc
Kasper Lahti <kasper.lahti@tink.se>
2019-08-14 11:16:50 +0200

    Add an Environment

---
+++ Sources/TinkLink/Environment.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+public enum Environment {
+    case production
+    case staging
+
+    var url: URL {
+        switch self {
+        case .production:
+            return URL(string: "main-grpc.production.oxford.tink.se:443")!
+        case .staging:
+            return URL(string: "main-grpc.staging.oxford.tink.se:443")!
+        }
+    }
+}
