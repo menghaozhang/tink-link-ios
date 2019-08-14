c69651855235ff375d21f5c58002a3f702923436
Kasper Lahti <kasper.lahti@tink.se>
2019-08-14 12:15:22 +0200

    Add credential service

---
+++ Sources/TinkLink/Services/CredentialService.swift
@@ -0,0 +1,17 @@
+import SwiftGRPC
+
+public final class CredentialService {
+    let channel: Channel
+
+    init(channel: Channel) {
+        self.channel = channel
+    }
+
+    private lazy var service = CredentialServiceServiceClient(channel: channel)
+
+    func credentials(completion: @escaping (Result<[GRPCCredential], Error>) -> Void) -> Cancellable {
+        let request = GRPCListCredentialsRequest()
+
+        return startCall(request: request, method: service.listCredentials, responseMap: { $0.credentials }, completion: completion)
+    }
+}
