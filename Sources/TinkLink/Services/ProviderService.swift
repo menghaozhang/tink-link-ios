1791289d50ea3534932f1eeee7413c006dc8dd28
Kasper Lahti <kasper.lahti@tink.se>
2019-08-14 11:24:20 +0200

    Return cancellable from request methods

--- Sources/TinkLink/Services/ProviderService.swift
+++ Sources/TinkLink/Services/ProviderService.swift
@@ -9,14 +9,16 @@ public final class ProviderService {
 
     private lazy var service = ProviderServiceServiceClient(channel: channel)
 
-    func providers(marketCode: String = "", capability: GRPCProvider.Capability = .unknown, includeTestType: Bool = false, completion: @escaping (Result<[GRPCProvider], Error>) -> Void) {
+    func providers(marketCode: String = "", capability: GRPCProvider.Capability = .unknown, includeTestType: Bool = false, completion: @escaping (Result<[GRPCProvider], Error>) -> Void) -> Cancellable {
         var request = GRPCProviderListRequest()
         request.marketCode = marketCode
         request.capability = capability
         request.includeTestType = includeTestType
 
+        let canceller = Cancellable()
+
         do {
-            try service.listProviders(request) { (response, result) in
+            canceller.call = try service.listProviders(request) { (response, result) in
                 if let response = response {
                     completion(.success(response.providers))
                 } else {
@@ -27,13 +29,17 @@ public final class ProviderService {
         } catch {
             completion(.failure(error))
         }
+
+        return canceller
     }
 
-    func providerMarkets(completion: @escaping (Result<[GRPCProviderMarket], Error>) -> Void) {
+    func providerMarkets(completion: @escaping (Result<[GRPCProviderMarket], Error>) -> Void) -> Cancellable {
         let request = GRPCProviderMarketListRequest()
 
+        let canceller = Cancellable()
+
         do {
-            try service.listProviderMarkets(request) { (response, result) in
+            canceller.call = try service.listProviderMarkets(request) { (response, result) in
                 if let response = response {
                     completion(.success(response.providerMarkets))
                 } else {
@@ -44,6 +50,8 @@ public final class ProviderService {
         } catch {
             completion(.failure(error))
         }
+
+        return canceller
     }
 
 }
