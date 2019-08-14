934f48997975fa901dc8fde15a2650c7b50f3116
Kasper Lahti <kasper.lahti@tink.se>
2019-08-14 11:23:24 +0200

    Request method documentation

--- Sources/TinkLink/Services/ProviderService.swift
+++ Sources/TinkLink/Services/ProviderService.swift
@@ -9,6 +9,14 @@ public final class ProviderService {
 
     private lazy var service = ProviderServiceServiceClient(channel: channel)
 
+    /// Lists all providers
+    ///
+    /// - Parameters:
+    ///   - marketCode: The market to fetch providers for. If no market is specified the providers for the users current market will be requested.
+    ///   - capability: Use the capability to only list providers with a specific capability. If no capability the provider response will not be filtered on capability.
+    ///   - includeTestType: If set to true, Providers of TEST type will be added in the response list. Defaults to false.
+    ///   - completion: The completion handler to call when the load request is complete.
+    /// - Returns: A Cancellable instance. Call cancel() on this instance if you no longer need the result of the request. Deinitializing this instance will also cancel the request.
     func providers(marketCode: String = "", capability: GRPCProvider.Capability = .unknown, includeTestType: Bool = false, completion: @escaping (Result<[GRPCProvider], Error>) -> Void) -> Cancellable {
         var request = GRPCProviderListRequest()
         request.marketCode = marketCode
@@ -33,6 +41,10 @@ public final class ProviderService {
         return canceller
     }
 
+    /// Lists all markets where there are providers available.
+    ///
+    /// - Parameter completion: The completion handler to call when the load request is complete.
+    /// - Returns: A Cancellable instance. Call cancel() on this instance if you no longer need the result of the request. Deinitializing this instance will also cancel the request.
     func providerMarkets(completion: @escaping (Result<[GRPCProviderMarket], Error>) -> Void) -> Cancellable {
         let request = GRPCProviderMarketListRequest()
 
