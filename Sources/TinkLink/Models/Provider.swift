e0bcae6a1227332ae0f6dbf9b3a0db402490bc09
Kasper Lahti <kasper.lahti@tink.se>
2019-08-15 09:16:34 +0200

    Make properties public

--- Sources/TinkLink/Models/Provider.swift
+++ Sources/TinkLink/Models/Provider.swift
@@ -2,12 +2,12 @@
 public struct Provider {
     /// The unique identifier of the provider.
     /// - Note: This is used when creating new credentials.
-    var name: String
+    public var name: String
 
     /// The display name of the provider.
-    var displayName: String
+    public var displayName: String
 
-    enum `Type` {
+    public enum `Type` {
         case unknown
         case bank
         case creditCard
@@ -18,9 +18,9 @@ public struct Provider {
     }
 
     /// Indicates what kind of financial institution the provider represents.
-    var type: `Type`
+    public var type: `Type`
 
-    enum Status {
+    public enum Status {
         case unknown
         case enabled
         case disabled
@@ -30,28 +30,28 @@ public struct Provider {
     
     /// Indicates the current status of the provider.
     /// - Note: It is only possible to perform credentials create or refresh actions on providers which are enabled.
-    var status: Status
+    public var status: Status
 
     // TODO: var credentialType: Credential.`Type`
 
-    var helpText: String
+    public var helpText: String
 
     /// Indicates if the provider is popular. This is normally set to true for the biggest financial institutions on a market.
-    var isPopular: Bool
+    public var isPopular: Bool
 
-    struct FieldSpecification {
+    public struct FieldSpecification {
         // TODO:
     }
 
-    var fields: [FieldSpecification]
+    public var fields: [FieldSpecification]
 
     /// A display name for providers which are branches of a bigger group.
-    var groupDisplayName: String
+    public var groupDisplayName: String
 
-    var image: URL?
+    public var image: URL?
 
     /// Short displayable description of the authentication type used.
-    var displayDescription: String
+    public var displayDescription: String
 
     /// Indicates what a provider is capable of.
     public struct Capabilities: OptionSet {
@@ -74,21 +74,21 @@ public struct Provider {
     }
 
     /// Indicates what this provider is capable of, in terms of financial data it can aggregate and if it can execute payments.
-    var capabilities: Capabilities
+    public var capabilities: Capabilities
 
-    enum AccessType {
+    public enum AccessType {
         case unknown
         case openBanking
         case other
     }
 
     /// What Tink uses to access the data.
-    var accessType: AccessType
+    public var accessType: AccessType
 
     /// The market of the provider.
     /// - Note: Each provider is unique per market.
-    var marketCode: String
+    public var marketCode: String
 
-    var financialInstitutionID: String
-    var financialInstitutionName: String
+    public var financialInstitutionID: String
+    public var financialInstitutionName: String
 }
