7bd3b0e79901a4c2b2bc7db2bb5309482f22ad45
Kasper Lahti <kasper.lahti@tink.se>
2019-08-20 16:45:54 +0200

    Infer service type

--- Sources/TinkLink/Client.swift
+++ Sources/TinkLink/Client.swift
@@ -21,6 +21,6 @@ public final class Client {
         }
     }
 
-    public private(set) lazy var providerService: ProviderService = ProviderService(channel: channel)
-    public private(set) lazy var credentialService: CredentialService = CredentialService(channel: channel)
+    public private(set) lazy var providerService = ProviderService(channel: channel)
+    public private(set) lazy var credentialService = CredentialService(channel: channel)
 }
