f49f88ab2f70d78f524f21a44ce064b9bc3d62db
Kasper Lahti <kasper.lahti@tink.se>
2019-08-23 09:11:18 +0200

    Documentation of the currency denominated amount type

--- Sources/TinkLink/Models/CurrencyDenominatedAmount.swift
+++ Sources/TinkLink/Models/CurrencyDenominatedAmount.swift
@@ -1,5 +1,8 @@
+/// An amount represented as a scale and unscaled value together with the ISO 4217 currency code of the amount.
 public struct CurrencyDenominatedAmount {
+    /// Amount represented as a scale and unscaled value together.
     var value: ExactNumber
+    /// The ISO 4217 currency code of the amount.
     var currencyCode: String
 }
 
