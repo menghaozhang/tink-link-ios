335aa97cef95b3c0c58fbafdda1a0e856eb623ef
Kasper Lahti <kasper.lahti@tink.se>
2019-08-27 13:25:59 +0200

    Add authentication token type

---
+++ Sources/TinkLink/Models/AuthenticationToken.swift
@@ -0,0 +1,11 @@
+public struct AuthenticationToken: Hashable, RawRepresentable {
+    public let rawValue: String
+
+    public init?(rawValue: String) {
+        self.rawValue = rawValue
+    }
+
+    public init(_ value: String) {
+        self.rawValue = value
+    }
+}
