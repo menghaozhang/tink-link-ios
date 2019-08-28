b104c17883ff5ba4859595029f5ad2985e7f0f9d
Menghao Zhang <menghao.zhang@tink.se>
2019-08-28 12:01:19 +0200

    Moved the codable from the `Client` object to the `TinkLink.Configuration` object

--- Examples/ProviderSelection/ProviderSelection/TinkLink/Client.swift
+++ Examples/ProviderSelection/ProviderSelection/TinkLink/Client.swift
@@ -3,24 +3,4 @@ import Foundation
 struct Client {
     let clientId: String
     let redirectUrl: URL
-    var timeoutIntervalForRequest: TimeInterval = 15
-}
-
-extension Client: Decodable {
-    enum CodingKeys: String, CodingKey {
-        case clientId = "TINK_CLIENT_ID"
-        case redirectUrl = "TINK_REDIRECT_URL"
-        case timeoutInterval = "TINK_TIMEOUT_INTERVAL"
-    }
-    
-    init(from decoder: Decoder) throws {
-        let values = try decoder.container(keyedBy: CodingKeys.self)
-        clientId = try values.decode(String.self, forKey: .clientId)
-        let redirectUrlString = try values.decode(String.self, forKey: .redirectUrl)
-        timeoutIntervalForRequest = try values.decode(Double.self, forKey: .timeoutInterval)
-        guard let redirectUrl = URL(string: redirectUrlString) else {
-            fatalError("Invalid URL")
-        }
-        self.redirectUrl = redirectUrl
-    }
 }
--- Examples/ProviderSelection/ProviderSelection/TinkLink/TinkLink.swift
+++ Examples/ProviderSelection/ProviderSelection/TinkLink/TinkLink.swift
@@ -1,14 +1,23 @@
 import Foundation
 public class TinkLink {
+    
+    public struct Configuration {
+        var clientId: String
+        var redirectUrl: URL
+        var timeoutIntervalForRequest: TimeInterval?
+    }
+    
     internal static let shared: TinkLink = TinkLink(client: TinkLink.client ?? fallbackClient)
     
     private static var client: Client?
+    private(set) public static var timeoutIntervalForRequest: TimeInterval = 15
     
     private static let fallbackClient: Client = {
         let fallbackUrl = Bundle.main.bundleURL
         do {
             let data = try Data(contentsOf: fallbackUrl)
-            return try PropertyListDecoder().decode(Client.self, from: data)
+            let configration = try PropertyListDecoder().decode(TinkLink.Configuration.self, from: data)
+            return Client(clientId: configration.clientId, redirectUrl: configration.redirectUrl)
         } catch {
             fatalError("Cannot find client")
         }
@@ -17,30 +26,46 @@ public class TinkLink {
     // Setup via configration files
     public static func configure(tinklinkUrl: URL) throws {
         let data = try Data(contentsOf: tinklinkUrl)
-        client = try PropertyListDecoder().decode(Client.self, from: data)
+        let configuration = try PropertyListDecoder().decode(TinkLink.Configuration.self, from: data)
+        configure(with: configuration)
     }
     
     // Setup via configration object
-    public static func configure(with configuration: TinkLinkConfiguration) {
+    public static func configure(with configuration: TinkLink.Configuration) {
         client = Client(clientId: configuration.clientId,
-                        redirectUrl: configuration.redirectUrl,
-                        timeoutIntervalForRequest: configuration.timeoutIntervalForRequest)
+                        redirectUrl: configuration.redirectUrl)
     }
     
-    //
+    // TODO: Some configurations can be changed after setup, for example timeoutIntervalForRequest and Qos, the changes should reflect to the stores and services
     public static func configure(timeoutInterval: TimeInterval) {
-        TinkLink.shared.client.timeoutIntervalForRequest = timeoutInterval
+        TinkLink.timeoutIntervalForRequest = timeoutInterval
     }
     
     private init(client: Client) {
         self.client = client
     }
     
-    var client: Client
+    let client: Client
+    var timeoutIntervalForRequest: TimeInterval {
+        return TinkLink.timeoutIntervalForRequest
+    }
 }
 
-public struct TinkLinkConfiguration {
-    var clientId: String
-    var redirectUrl: URL
-    var timeoutIntervalForRequest: TimeInterval
+extension TinkLink.Configuration: Decodable {
+    enum CodingKeys: String, CodingKey {
+        case clientId = "TINK_CLIENT_ID"
+        case redirectUrl = "TINK_REDIRECT_URL"
+        case timeoutInterval = "TINK_TIMEOUT_INTERVAL"
+    }
+    
+    public init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        clientId = try values.decode(String.self, forKey: .clientId)
+        timeoutIntervalForRequest = try? values.decode(Double.self, forKey: .timeoutInterval)
+        let redirectUrlString = try values.decode(String.self, forKey: .redirectUrl)
+        guard let redirectUrl = URL(string: redirectUrlString) else {
+            fatalError("Invalid URL")
+        }
+        self.redirectUrl = redirectUrl
+    }
 }
