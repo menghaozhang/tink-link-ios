696224440cdad340fcfdf4d48e77441b9785ef27
Menghao Zhang <menghao.zhang@tink.se>
2019-09-03 16:29:58 +0200

    Added and updated access control for field validator and credential context

---
+++ Sources/TinkLink/Models/FieldsValidator.swift
@@ -0,0 +1,51 @@
+import Foundation
+
+extension Provider.FieldSpecification {
+    public func validatedValue() -> Result<String, FieldSpecificationError> {
+        let value = self.value
+        if value.isEmpty, !self.isOptional {
+            return .failure(.requiredFieldEmptyValue(fieldName: self.name))
+        } else if let maxLength = self.maxLength, maxLength > 0 && maxLength < value.count {
+            return .failure(.maxLengthLimit(fieldName: self.name, maxLength: maxLength))
+        } else if let minLength = self.minLength, minLength > 0 && minLength > value.count {
+            return .failure(.minLengthLimit(fieldName: self.name, minLength: minLength))
+        } else if !self.pattern.isEmpty, let regex = try? NSRegularExpression(pattern: self.pattern, options: []) {
+            let range = regex.rangeOfFirstMatch(in: value, options: [], range: NSRange(location: 0, length: value.count))
+            if range.location == NSNotFound {
+                return .failure(.validationFailed(fieldName: self.name, patternError: self.patternError))
+            }
+        }
+        return .success(value)
+    }
+}
+
+extension Array where Element == Provider.FieldSpecification {
+    func createCredentialValues() -> Result<[String: String], FieldSpecificationsError> {
+        var fieldSpecificationsError = FieldSpecificationsError(errors: [])
+        var fieldValus = [String: String]()
+        self.forEach {
+            switch $0.validatedValue() {
+            case .failure(let error):
+                fieldSpecificationsError.errors.append(error)
+            case .success(let value):
+                fieldValus[$0.name] = value
+            }
+        }
+        if fieldSpecificationsError.errors.count > 0 {
+            return .failure(fieldSpecificationsError)
+        }
+        return .success(fieldValus)
+    }
+}
+
+public enum FieldSpecificationError: Error {
+    case invalidField(String)
+    case validationFailed(fieldName: String, patternError: String)
+    case maxLengthLimit(fieldName: String, maxLength: Int)
+    case minLengthLimit(fieldName: String, minLength: Int)
+    case requiredFieldEmptyValue(fieldName: String)
+}
+
+struct FieldSpecificationsError: Error {
+    var errors: [FieldSpecificationError]
+}
---
+++ Sources/TinkLink/Stores/CredentialContext.swift
@@ -0,0 +1,98 @@
+import Foundation
+
+public class SupplementInformationTask {
+    public init(credentialContext: CredentialContext, credential: Credential) {
+        self.credential = credential
+        fields = credential.supplementalInformationFields
+        self.credentialContext = credentialContext
+    }
+    private weak var credentialContext: CredentialContext?
+    private var credential: Credential
+    public var fields: [Provider.FieldSpecification]
+    
+    public func submit() {
+        if let fields = try? fields.createCredentialValues().get() {
+            credentialContext?.addSupplementalInformation(for: credential, supplementalInformationFields: fields)
+        }
+    }
+    
+    public func cancel() {
+        credentialContext?.cancelSupplementInformation(for: credential)
+    }
+}
+
+public class CredentialContext {
+    public enum AddCredentialStatus {
+        case created
+        case authenticating
+        case updating(status: String)
+        case awaitingSupplementalInformation(SupplementInformationTask)
+        case awaitingThirdPartyAppAuthentication(URL)
+    }
+    
+    public private(set) var credentials: [Identifier<Credential>: Credential] = [:]
+    private let credentialStore = CredentialStore.shared
+    private let storeObserverToken = StoreObserverToken()
+    
+    private var progressHandlers: [Identifier<Credential>: (AddCredentialStatus) -> Void] = [:]
+    private var completions: [Identifier<Credential>: (Result<Credential, Error>) -> Void] = [:]
+    
+    public init() {
+        credentials = credentialStore.credentials
+        credentialStore.addCredentialsObserver(token: storeObserverToken) { [weak self] tokenId in
+            guard let strongSelf = self, strongSelf.storeObserverToken.has(id: tokenId) else {
+                return
+            }
+            strongSelf.credentials.forEach({ (key, value) in
+                if let credential = strongSelf.credentialStore.credentials[key] {
+                    if value.status != credential.status {
+                        strongSelf.handleUpdate(for: credential)
+                    }
+                }
+            })
+            strongSelf.credentials = strongSelf.credentialStore.credentials
+        }
+    }
+    
+    public func addCredential(for provider: Provider, fields: [String: String], progressHandler: @escaping (AddCredentialStatus) -> Void,  completion: @escaping(Result<Credential, Error>) -> Void) {
+        credentialStore.addCredential(for: provider, fields: fields) { [weak self] result in
+            guard let strongSelf = self else { return }
+            switch result {
+            case .failure(let error):
+                completion(.failure(error))
+            case .success(let credential):
+                strongSelf.progressHandlers[credential.id] = progressHandler
+                strongSelf.completions[credential.id] = completion
+            }
+        }
+    }
+    
+    private func handleUpdate(for credential: Credential) {
+        guard let progressHandler = progressHandlers[credential.id], let completion = completions[credential.id] else { return }
+        switch credential.status {
+        case .created:
+            progressHandler(.created)
+        case .authenticating:
+            progressHandler(.authenticating)
+        case .awaitingSupplementalInformation:
+            let supplementInformationTask = SupplementInformationTask(credentialContext: self, credential: credential)
+            progressHandler(.awaitingSupplementalInformation(supplementInformationTask))
+        case .awaitingThirdPartyAppAuthentication:
+            progressHandler(.awaitingThirdPartyAppAuthentication(URL(string: "https://www.google.com")!))
+        case .updating:
+            progressHandler(.updating(status: "fetching transaction"))
+        case .updated:
+            completion(.success(credential))
+        default:
+            completion(.failure(NSError()))
+        }
+    }
+    
+    fileprivate func addSupplementalInformation(for credential: Credential, supplementalInformationFields: [String: String]) {
+        credentialStore.addSupplementalInformation(for: credential, supplementalInformationFields: supplementalInformationFields)
+    }
+    
+    fileprivate func cancelSupplementInformation(for credential: Credential) {
+        credentialStore.cancelSupplementInformation(for: credential)
+    }
+}
