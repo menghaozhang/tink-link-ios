57f4e6eb7998be46f1d29dbf29c3b5ab80e8bfe4
Kasper Lahti <kasper.lahti@tink.se>
2019-09-09 15:15:04 +0200

    Move task to separate file

--- Sources/TinkLink/Context/CredentialContext.swift
+++ Sources/TinkLink/Context/CredentialContext.swift
@@ -1,26 +1,5 @@
 import Foundation
 
-public class SupplementInformationTask {
-    public init(credentialContext: CredentialContext, credential: Credential) {
-        self.credential = credential
-        fields = credential.supplementalInformationFields
-        self.credentialContext = credentialContext
-    }
-
-    private weak var credentialContext: CredentialContext?
-
-    public private(set) var credential: Credential
-    public var fields: [Provider.FieldSpecification]
-    
-    public func submit() {
-        credentialContext?.addSupplementalInformation(for: credential, supplementalInformationFields: fields)
-    }
-    
-    public func cancel() {
-        credentialContext?.cancelSupplementInformation(for: credential)
-    }
-}
-
 public class CredentialContext {
     public enum AddCredentialStatus {
         case created
