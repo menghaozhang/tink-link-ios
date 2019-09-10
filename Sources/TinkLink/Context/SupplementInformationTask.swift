d2ffef8234571f188d01f43971c23c6ccf854a1f
Kasper Lahti <kasper.lahti@tink.se>
2019-09-10 09:45:41 +0200

    Move properties above initializer

--- Sources/TinkLink/Context/SupplementInformationTask.swift
+++ Sources/TinkLink/Context/SupplementInformationTask.swift
@@ -1,13 +1,13 @@
 public class SupplementInformationTask {
     private let credentialStore = CredentialStore.shared
 
+    public private(set) var credential: Credential
+    public var fields: [Provider.FieldSpecification]
+
     public init(credential: Credential) {
         self.credential = credential
         fields = credential.supplementalInformationFields
     }
-
-    public private(set) var credential: Credential
-    public var fields: [Provider.FieldSpecification]
 
     public func submit() {
         credentialStore.addSupplementalInformation(for: credential, supplementalInformationFields: fields)
