6dd0ffc4296599ef10a28f6b71e7a73ffc48d556
Kasper Lahti <kasper.lahti@tink.se>
2019-09-10 15:58:17 +0200

    Remove configuration example from ProviderSelection project

--- Examples/ProviderSelection/ProviderSelection/AppDelegate.swift
+++ Examples/ProviderSelection/ProviderSelection/AppDelegate.swift
@@ -17,14 +17,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        // Can support two options to setup the TinkLink
-        // Option 1
-        let tinkLinkConfiguration = TinkLink.Configuration(environment: .staging, clientId: "123", redirectUrl: URL(string: "http://localhost:3000")!)
-        TinkLink.configure(with: tinkLinkConfiguration)
-        
-        // Option 2
-//        try? TinkLink.configure(tinklinkUrl: Bundle.main.url(forResource: "Tinklink", withExtension: "plist")!)
-
         window = UIWindow(frame: UIScreen.main.bounds)
         let providerMarketsViewController = ProviderMarketsViewController()
         let navigationController = UINavigationController(rootViewController: providerMarketsViewController)
