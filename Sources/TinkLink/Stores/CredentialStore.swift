b356e844ce133263ff3ba3d022ef40a9e9f1434e
Kasper Lahti <kasper.lahti@tink.se>
2019-09-10 09:18:54 +0200

    Return canceller from store

--- Sources/TinkLink/Stores/CredentialStore.swift
+++ Sources/TinkLink/Stores/CredentialStore.swift
@@ -22,11 +22,11 @@ final class CredentialStore {
         service = TinkLink.shared.client.credentialService
     }
     
-    func addCredential(for provider: Provider, fields: [Provider.FieldSpecification], completion: @escaping(Result<Credential, Error>) -> Void) {
-        guard createCredentialCanceller[provider.name] == nil else {
-            return
+    func addCredential(for provider: Provider, fields: [Provider.FieldSpecification], completion: @escaping(Result<Credential, Error>) -> Void) -> Cancellable {
+        if let canceller = createCredentialCanceller[provider.name] {
+            return canceller
         }
-        createCredentialCanceller[provider.name] = service.createCredential(providerName: provider.name, fields: fields.makeFields(), completion: { [weak self, provider] (result) in
+        let canceller = service.createCredential(providerName: provider.name, fields: fields.makeFields(), completion: { [weak self, provider] (result) in
             guard let strongSelf = self else { return }
             DispatchQueue.main.async {
                 do {
@@ -40,6 +40,8 @@ final class CredentialStore {
                 strongSelf.createCredentialCanceller[provider.name] = nil
             }
         })
+        createCredentialCanceller[provider.name] = canceller
+        return canceller
     }
     
     func addSupplementalInformation(for credential: Credential, supplementalInformationFields: [Provider.FieldSpecification]) {
