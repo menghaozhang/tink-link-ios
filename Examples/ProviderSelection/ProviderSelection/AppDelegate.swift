9d1e921308e513820f70d922d69b3cbd58a65a39
Kasper Lahti <kasper.lahti@tink.se>
2019-09-13 12:39:38 +0200

    Whitespace

--- Examples/ProviderSelection/ProviderSelection/AppDelegate.swift
+++ Examples/ProviderSelection/ProviderSelection/AppDelegate.swift
@@ -16,11 +16,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
         window = UIWindow(frame: UIScreen.main.bounds)
+
         let providerMarketsViewController = ProviderMarketsViewController()
         let navigationController = UINavigationController(rootViewController: providerMarketsViewController)
         navigationController.navigationBar.prefersLargeTitles = true
+
         window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
 
