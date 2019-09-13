1dfdca97ffbd5397a73034441cf0e230c8477d68
Kasper Lahti <kasper.lahti@tink.se>
2019-09-13 12:25:22 +0200

    Remove redundant numberOfSections method

--- Examples/ProviderSelection/ProviderSelection/ProviderViewControllers/ProviderListViewController.swift
+++ Examples/ProviderSelection/ProviderSelection/ProviderViewControllers/ProviderListViewController.swift
@@ -55,10 +55,6 @@ extension ProviderListViewController {
 
 // MARK: - UITableViewDataSource
 extension ProviderListViewController {
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return providerGroups.count
     }
