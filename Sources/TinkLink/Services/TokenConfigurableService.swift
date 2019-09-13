1a705ea3c5535c98f86d1d17aac46decb8623c62
Menghao Zhang <menghao.zhang@tink.se>
2019-09-13 10:33:41 +0200

    Updated the config to configuration

--- Sources/TinkLink/Services/TokenConfigurableService.swift
+++ Sources/TinkLink/Services/TokenConfigurableService.swift
@@ -1,18 +1,18 @@
 import SwiftGRPC
 
 protocol TokenConfigurableServiceBase {
-    func config(_ accessToken: AccessToken)
+    func configure(_ accessToken: AccessToken)
 }
 
 protocol TokenConfigurableService: TokenConfigurableServiceBase {
     associatedtype ServiceClient: ServiceClientBase
     
     var service: ServiceClient { get set }
-    func config(_ accessToken: AccessToken)
+    func configure(_ accessToken: AccessToken)
 }
 
 extension TokenConfigurableService {
-    func config(_ accessToken: AccessToken) {
+    func configure(_ accessToken: AccessToken) {
         do {
             try service.metadata.addAccessToken(accessToken.rawValue)
         } catch {
