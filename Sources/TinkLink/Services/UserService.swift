b70a6ac6e56ccc88a0bc32c01beee283c58345fd
Menghao Zhang <menghao.zhang@tink.se>
2019-09-16 11:40:07 +0200

    Added the TODO, and used the Swedish locale for now

--- Sources/TinkLink/Services/UserService.swift
+++ Sources/TinkLink/Services/UserService.swift
@@ -21,7 +21,8 @@ public final class UserService {
     public func createAnonymous(market: Market? = nil, locale: Locale = .current, origin: String? = nil, completion: @escaping (Result<AccessToken, Error>) -> Void) -> Cancellable {
         var request = GRPCCreateAnonymousRequest()
         request.market = market?.code ?? ""
-        request.locale = locale.identifier
+        // TODO: Use the correct/acceptable locale
+        request.locale = "sv_SE"
         request.origin = origin ?? ""
 
         return startCall(for: request, method: service.createAnonymous, responseMap: { AccessToken($0.accessToken) }, completion: completion)
