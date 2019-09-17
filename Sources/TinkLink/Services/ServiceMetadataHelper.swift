dffa9de720eafc7a7736b0506551658cd96bc4e1
Kasper Lahti <kasper.lahti@tink.se>
2019-09-17 10:46:19 +0200

    Don't configure client key and client id in addTinkMetadata helper

--- Sources/TinkLink/Services/ServiceMetadataHelper.swift
+++ Sources/TinkLink/Services/ServiceMetadataHelper.swift
@@ -54,17 +54,11 @@ extension Metadata {
     
     func addTinkMetadata() throws {
         let info = ProcessInfo.processInfo
-        if let clientKey = info.tinkClientKey {
-            try add(key: HeaderKeys.clientKey.key, value: clientKey)
-        }
         if let deviceID = info.tinkDeviceID {
             try add(key: HeaderKeys.deviceId.key, value: deviceID)
         }
         if let sessionID = info.tinkSessionID {
             try add(key: HeaderKeys.authorization.key, value: "Session \(sessionID)")
-        }
-        if let oAuthClientID = info.tinkOAuthClientID {
-            try add(key: HeaderKeys.clientId.key, value: oAuthClientID)
         }
         let authorization = dictionaryRepresentation[HeaderKeys.authorization.key]
         try addAccessToken(authorization)
