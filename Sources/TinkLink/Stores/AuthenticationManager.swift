8fa94861917b89bec8e7ad5f934f05e19564ec2b
Kasper Lahti <kasper.lahti@tink.se>
2019-09-17 08:52:52 +0200

    Change create anonymous error handling so reauthentication only happens if unauthenticated for now

--- Sources/TinkLink/Stores/AuthenticationManager.swift
+++ Sources/TinkLink/Stores/AuthenticationManager.swift
@@ -1,4 +1,5 @@
 import Foundation
+import SwiftGRPC
 
 final class AuthenticationManager {
     static let shared = AuthenticationManager()
@@ -10,21 +11,32 @@ final class AuthenticationManager {
         service = TinkLink.shared.client.userService
     }
     
-    func authenticateIfNeeded<Service: TokenConfigurableServiceBase>(service otherService: Service, for market: Market = Market(code: "SE"), completion: @escaping (AccessToken) -> Void) {
+    func authenticateIfNeeded<Service: TokenConfigurableServiceBase>(service otherService: Service, for market: Market = Market(code: "SE"), completion: @escaping (Result<AccessToken, Error>) -> Void) {
         if let accessToken = accessToken {
             otherService.configure(accessToken)
-            completion(accessToken)
+            self.accessToken = accessToken
+            completion(.success(accessToken))
         } else {
             guard cancellable == nil else { return }
             cancellable = service.createAnonymous(market: Market(code: "SE")) { [weak self] result in
                 guard let self = self else { return }
-                if let accessToken = try? result.get() {
+                do {
+                    let accessToken = try result.get()
                     otherService.configure(accessToken)
                     self.accessToken = accessToken
-                    completion(accessToken)
-                } else {
-                    // TODO: Auto retry? Maybe should use some auto retry handler for this same as the credential status polling
-                    self.authenticateIfNeeded(service: otherService, for: market, completion: completion)
+                    completion(.success(accessToken))
+                } catch let error as RPCError {
+                    if let callResult = error.callResult {
+                        switch callResult.statusCode {
+                        case .unauthenticated:
+                            // TODO: Auto retry? Maybe should use some auto retry handler for this same as the credential status polling
+                            self.authenticateIfNeeded(service: otherService, for: market, completion: completion)
+                        default:
+                            completion(.failure(error))
+                        }
+                    }
+                } catch {
+                    completion(.failure(error))
                 }
                 self.cancellable = nil
             }
