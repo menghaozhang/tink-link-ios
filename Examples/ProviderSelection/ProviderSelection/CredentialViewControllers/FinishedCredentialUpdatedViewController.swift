53cf008c8e66a235fe000bd2af1f4b1c99600a22
Lukas Lipka <lukaslipka@gmail.com>
2019-09-17 14:18:43 +0200

    Convert AccountContext into a FIXME.

--- Examples/ProviderSelection/ProviderSelection/CredentialViewControllers/FinishedCredentialUpdatedViewController.swift
+++ Examples/ProviderSelection/ProviderSelection/CredentialViewControllers/FinishedCredentialUpdatedViewController.swift
@@ -1,16 +1,21 @@
 import UIKit
 import TinkLink
 
-final class FinishedCredentialUpdatedViewController: UITableViewController, AccountContextDelegate {
+struct Account {
+    let name: String
+    let balance: CurrencyDenominatedAmount
+}
+
+final class FinishedCredentialUpdatedViewController: UITableViewController {
     var credential: Credential
-    var accountContext: AccountContext
     var accounts: [Account] = []
     var numberFormatter: NumberFormatter = NumberFormatter()
     
     init(credential: Credential) {
         self.credential = credential
-        accountContext = AccountContext()
         super.init(style: .grouped)
+
+        // FIXME: Accounts need to be fetched using a customer backend
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -19,9 +24,7 @@ final class FinishedCredentialUpdatedViewController: UITableViewController, Acco
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        accountContext.delegate = self
-        
+
         numberFormatter.maximumFractionDigits = 2
         numberFormatter.minimumFractionDigits = 0
         
@@ -45,16 +48,5 @@ final class FinishedCredentialUpdatedViewController: UITableViewController, Acco
         cell.textLabel?.text = accounts[indexPath.item].name
         cell.detailTextLabel?.text = numberFormatter.string(from: NSNumber(value: accounts[indexPath.item].balance.value.doubleValue))
         return cell
-    }
-    
-    // MARK: - AccountContextDelegate
-    func accountContext(_ store: AccountContext, didUpdateAccounts accounts: [Identifier<Credential> : [Account]]) {
-        self.accounts = accounts[credential.id] ?? []
-        tableView.reloadData()
-        navigationItem.rightBarButtonItem = nil
-    }
-    
-    func accountContext(_ store: AccountContext, didReceiveError error: Error) {
-        // TODO: Handle Error
     }
 }
