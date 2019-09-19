006dbaf2f3e35bba8b55b5c6f04a3902bee28b62
Kasper Lahti <kasper.lahti@tink.se>
2019-09-19 12:49:25 +0200

    Document unauthenticatedPerformFetchProviders method and add precondition

--- Sources/TinkLink/Stores/ProviderStore.swift
+++ Sources/TinkLink/Stores/ProviderStore.swift
@@ -61,7 +61,12 @@ final class ProviderStore {
         return multiCanceller
     }
 
+    /// Requests providers for a market.
+    ///
+    /// - Parameter attributes: Attributes for providers to fetch
+    /// - Precondition: Service should be configured with access token before this method is called.
     private func unauthenticatedPerformFetchProviders(attributes: ProviderContext.Attributes) -> Cancellable {
+        precondition(service.metadata[Metadata.HeaderKeys.authorization.key] != nil, "Service doesn't have authentication metadata set!")
         return service.providers(market: attributes.market, capabilities: attributes.capabilities, includeTestProviders: attributes.includeTestProviders) { [weak self, attributes] result in
             guard let self = self else { return }
             DispatchQueue.main.async {
