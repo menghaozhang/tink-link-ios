55611aaafd1a062c2dc2d11e5d86f6931f3118fe
Kasper Lahti <kasper.lahti@tink.se>
2019-09-19 12:39:32 +0200

    Remove unnecessary AnyCancellable

--- Sources/TinkLink/Services/MultiCanceller.swift
+++ Sources/TinkLink/Services/MultiCanceller.swift
@@ -1,22 +1,10 @@
-private class AnyCancellable {
-    private var cancellable: Cancellable
-
-    init(cancellable: Cancellable) {
-        self.cancellable = cancellable
-    }
-
-    func cancel() {
-        cancellable.cancel()
-    }
-}
-
 class MultiCanceller: Cancellable {
-    private var cancellers: [AnyCancellable] = []
+    private var cancellers: [Cancellable] = []
 
     private(set) var isCancelled: Bool = false
 
     func add(_ canceller: Cancellable) {
-        cancellers.append(AnyCancellable(cancellable: canceller))
+        cancellers.append(canceller)
     }
 
     func cancel() {
