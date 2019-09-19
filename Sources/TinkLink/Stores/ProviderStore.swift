ec6520c7cd6b601b86b63758e55a8d6153a3b49a
Kasper Lahti <kasper.lahti@tink.se>
2019-09-19 10:08:36 +0200

    Use multi canceller to keep cancellation handlers for both authentication request and providers fetch

--- Sources/TinkLink/Stores/ProviderStore.swift
+++ Sources/TinkLink/Stores/ProviderStore.swift
@@ -1,4 +1,5 @@
 import Foundation
+import SwiftGRPC
 
 final class ProviderStore {
     static let shared: ProviderStore = ProviderStore()
@@ -33,24 +34,36 @@ final class ProviderStore {
     }
 
     func performFetchProvidersIfNeeded(for attributes: ProviderContext.Attributes) {
-        authenticationManager.authenticateIfNeeded(service: service, for: market, locale: locale) { [weak self] _ in
-            guard let self = self, self.providerFetchCancellers[attributes] == nil else {
-                return
-            }
-            let cancellable = self.service.providers(market: attributes.market, capabilities: attributes.capabilities, includeTestProviders: attributes.includeTestProviders) { [attributes] result in
-                DispatchQueue.main.async {
-                    switch result {
-                    case .success(let fetchedProviders):
-                        let filteredProviders = fetchedProviders.filter({ attributes.accessTypes.contains($0.accessType) })
-                        self.providerMarketGroups[attributes.market] = .success(filteredProviders)
-                    case .failure(let error):
-                        self.providerMarketGroups[attributes.market] = .failure(error)
+        var multiCanceller = MultiCanceller()
+        
+        let authCanceller = authenticationManager.authenticateIfNeeded(service: service, for: market, locale: locale) { [weak self] authenticationResult in
+            guard let self = self, !multiCanceller.isCancelled else { return }
+            do {
+                try authenticationResult.get()
+
+                let cancellable = self.service.providers(market: attributes.market, capabilities: attributes.capabilities, includeTestProviders: attributes.includeTestProviders) { [attributes] result in
+                    DispatchQueue.main.async {
+                        switch result {
+                        case .success(let fetchedProviders):
+                            let filteredProviders = fetchedProviders.filter({ attributes.accessTypes.contains($0.accessType) })
+                            self.providerMarketGroups[attributes.market] = .success(filteredProviders)
+                        case .failure(let error):
+                            self.providerMarketGroups[attributes.market] = .failure(error)
+                        }
+                        self.providerFetchCancellers[attributes] = nil
                     }
-                    self.providerFetchCancellers[attributes] = nil
                 }
+                multiCanceller.add(cancellable)
+            } catch {
+                self.providerMarketGroups[attributes.market] = .failure(error)
+                self.providerFetchCancellers[attributes] = nil
             }
-            self.providerFetchCancellers[attributes] = cancellable
         }
+        if let canceller = authCanceller {
+            multiCanceller.add(canceller)
+        }
+
+        providerFetchCancellers[attributes] = multiCanceller
     }
     
     func performFetchMarketsIfNeeded() {
