828f22c52849f82bfc61d2550473b9ae50833527
Kasper Lahti <kasper.lahti@tink.se>
2019-09-23 16:02:15 +0200

    Use system items for cancel button items

--- Examples/ProviderSelection/ProviderSelection/CredentialViewControllers/SupplementalInformationViewController.swift
+++ Examples/ProviderSelection/ProviderSelection/CredentialViewControllers/SupplementalInformationViewController.swift
@@ -42,7 +42,7 @@ extension SupplementalInformationViewController {
         
         navigationItem.title = "Enter Supplemental Information"
         navigationItem.largeTitleDisplayMode = .never
-        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(cancelButtonPressed(_:)))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(cancelButtonPressed(_:)))
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Submit", style: .done, target: self, action: #selector(doneButtonPressed(_:)))
         navigationItem.rightBarButtonItem?.isEnabled = false
     }
