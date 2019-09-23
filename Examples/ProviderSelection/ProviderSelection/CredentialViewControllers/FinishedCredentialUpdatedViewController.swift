681ea6c413c80dd82912f80dd1f6374eff07bcc4
Kasper Lahti <kasper.lahti@tink.se>
2019-09-23 14:17:25 +0200

    Whitespace

--- Examples/ProviderSelection/ProviderSelection/CredentialViewControllers/FinishedCredentialUpdatedViewController.swift
+++ Examples/ProviderSelection/ProviderSelection/CredentialViewControllers/FinishedCredentialUpdatedViewController.swift
@@ -9,11 +9,11 @@ final class FinishedCredentialUpdatedViewController: UITableViewController {
         self.credential = credential
         super.init(style: .grouped)
     }
-    
+
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
