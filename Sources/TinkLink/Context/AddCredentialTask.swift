c0aeb85b1ea7588aa944381e9e981bac8969d273
Kasper Lahti <kasper.lahti@tink.se>
2019-09-25 17:17:44 +0200

    Add some documentation

--- Sources/TinkLink/Context/AddCredentialTask.swift
+++ Sources/TinkLink/Context/AddCredentialTask.swift
@@ -1,5 +1,8 @@
 import Foundation
 
+/// A task that manages progress of adding a credential.
+///
+/// Use `CredentialContext` to create a task.
 public class AddCredentialTask {
     /// Indicates the state of a credential being added.
     ///
@@ -31,10 +34,19 @@ public class AddCredentialTask {
 
     private(set) var credential: Credential?
 
+    /// Cases to evaluate when credential status changes.
+    ///
+    /// Use with `CredentialContext.addCredential(for:form:completionPredicate:progressHandler:completion:)` to set when add credential task should call completion handler if successful.
     public enum CompletionPredicate {
+        /// A predicate that indicates the credential's status is `updating`.
         case updating
+        /// A predicate that indicates the credential's status is `updated`.
         case updated
     }
+
+    /// Predicate for when credential task is completed.
+    ///
+    /// Task will execute it's completion handler if the credential's status changes to match this predicate.
     public let completionPredicate: CompletionPredicate
 
     let progressHandler: (Status) -> Void
@@ -61,6 +73,7 @@ public class AddCredentialTask {
         credentialStatusPollingTask?.pollStatus()
     }
 
+    /// Cancel the task.
     public func cancel() {
         callCanceller?.cancel()
     }
