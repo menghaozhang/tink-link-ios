fdd3745bda99c80a065084fb0ae2caeb2fbd7f1c
Kasper Lahti <kasper.lahti@tink.se>
2019-09-30 08:35:23 +0200

    Change how TinkLink singleton and separate TinkLink instances are setup.

--- Sources/TinkLink/TinkLink.swift
+++ Sources/TinkLink/TinkLink.swift
@@ -1,66 +1,55 @@
 import Foundation
 
 public class TinkLink {
-    
-    public init() {}
     public static let shared: TinkLink = TinkLink()
-    
+
+    private var configuration: Configuration!
+
+    private(set) lazy var client = Client(configuration: configuration)
+
     lazy var providerStore = ProviderStore(tinkLink: self)
     lazy var credentialStore = CredentialStore(tinkLink: self)
     lazy var authenticationManager = AuthenticationManager(tinkLink: self)
-    
-    private var _client: Client?
-    private(set) var client: Client {
-        get {
-            if let client = _client {
-                return client
-            } else if let fallbackUrl = Bundle.main.url(forResource: "Info", withExtension: "plist") {
+
+    private init() {
+        if let fallbackUrl = Bundle.main.url(forResource: "Info", withExtension: "plist") {
+            do {
+                let data = try Data(contentsOf: fallbackUrl)
+                self.configuration = try PropertyListDecoder().decode(TinkLink.Configuration.self, from: data)
+            } catch {
                 do {
-                    _client = try Client(configurationUrl: fallbackUrl)
-                } catch {
-                    do {
-                        let client = try Client(processInfo: .processInfo)
-                        _client = client
-                    } catch {
-                        fatalError(error.localizedDescription)
-                    }
-                }
-                return _client!
-            } else {
-                do {
-                    let client = try Client(processInfo: .processInfo)
-                    _client = client
-                    return _client!
+                    self.configuration = try Configuration(processInfo: .processInfo)
                 } catch {
                     fatalError(error.localizedDescription)
                 }
             }
-        }
-        set {
-            _client = newValue
+        } else {
+            do {
+                self.configuration = try Configuration(processInfo: .processInfo)
+            } catch {
+                fatalError(error.localizedDescription)
+            }
         }
     }
-    
+
+    public init(configuration: Configuration) {
+        self.client = Client(environment: configuration.environment , clientID: configuration.clientID, certificateURL: configuration.certificateURL, market: configuration.market, locale: configuration.locale)
+    }
+
+    public convenience init(configurationPlistURL url: URL) throws {
+        let data = try Data(contentsOf: url)
+        let configuration = try PropertyListDecoder().decode(TinkLink.Configuration.self, from: data)
+        self.init(configuration: configuration)
+    }
+
     // Setup via configration files
     public static func configure(tinklinkUrl: URL) throws {
         let data = try Data(contentsOf: tinklinkUrl)
-        let configuration = try PropertyListDecoder().decode(TinkLink.Configuration.self, from: data)
-        configure(with: configuration)
+        shared.configuration = try PropertyListDecoder().decode(TinkLink.Configuration.self, from: data)
     }
-    
+
     // Setup via configration object
     public static func configure(with configuration: TinkLink.Configuration) {
-        shared._client = Client(environment: configuration.environment , clientID: configuration.clientID, certificateURL: configuration.certificateURL, market: configuration.market, locale: configuration.locale)
-    }
-    
-    // Used to setup additional TinkLink object
-    public func configure(tinklinkUrl: URL) throws {
-        let data = try Data(contentsOf: tinklinkUrl)
-        let configuration = try PropertyListDecoder().decode(TinkLink.Configuration.self, from: data)
-        configure(with: configuration)
-    }
-    
-    public func configure(with configuration: TinkLink.Configuration) {
-        _client = Client(environment: configuration.environment , clientID: configuration.clientID, certificateURL: configuration.certificateURL, market: configuration.market, locale: configuration.locale)
+        shared.configuration = configuration
     }
 }
