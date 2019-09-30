1258a686c09b4768aa5568b88dd0e22012198971
Kasper Lahti <kasper.lahti@tink.se>
2019-09-30 08:16:59 +0200

    Add market configuration test

--- Tests/TinkLinkTests/ConfigurationTests.swift
+++ Tests/TinkLinkTests/ConfigurationTests.swift
@@ -7,4 +7,11 @@ class ConfigurationTests: XCTestCase {
         let link = TinkLink(configuration: configuration)
         XCTAssertNotNil(link.configuration)
     }
+
+    func testMarketConfiguration() {
+        let configuration = TinkLink.Configuration(clientID: "abc", market: "SE")
+        let link = TinkLink(configuration: configuration)
+        XCTAssertEqual(link.configuration.market.rawValue, "SE")
+        XCTAssertEqual(link.client.market.rawValue, "SE")
+    }
 }
