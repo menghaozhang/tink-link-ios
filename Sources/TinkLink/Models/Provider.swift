824b84d5eaf088b2d8ea24ab525910ade6d38106
Kasper Lahti <kasper.lahti@tink.se>
2019-10-07 16:33:29 +0200

    Docs for financial institution fields

--- Sources/TinkLink/Models/Provider.swift
+++ Sources/TinkLink/Models/Provider.swift
@@ -112,7 +112,10 @@ public struct Provider {
     /// - Note: Each provider is unique per market.
     public var marketCode: String
 
+    /// A unique identifier to group providers belonging the same financial institution.
     public var financialInstitutionID: String
+
+    /// The name of the financial institution.
     public var financialInstitutionName: String
 }
 
