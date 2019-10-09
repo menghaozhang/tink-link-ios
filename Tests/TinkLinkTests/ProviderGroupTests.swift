b47c0feb8709de558c81e9f39ee62cdc891ccc20
Kasper Lahti <kasper.lahti@tink.se>
2019-10-09 09:39:14 +0200

    Test providers

--- Tests/TinkLinkTests/ProviderGroupTests.swift
+++ Tests/TinkLinkTests/ProviderGroupTests.swift
@@ -2,4 +2,43 @@ import XCTest
 @testable import TinkLink
 
 class ProviderGroupTests: XCTestCase {
+    func testGroup() {
+        let nordeaBankID = Provider(
+            id: "nordea-bankid",
+            displayName: "Nordea",
+            type: .bank,
+            status: .enabled,
+            credentialType: .mobileBankID,
+            helpText: "",
+            isPopular: true,
+            fields: [],
+            groupDisplayName: "Nordea",
+            image: nil,
+            displayDescription: "Mobile BankID",
+            capabilities: .init(rawValue: 1266),
+            accessType: .other,
+            marketCode: "SE",
+            financialInstitutionID: "dde2463acf40501389de4fca5a3693a4",
+            financialInstitutionName: "Nordea"
+        )
+
+        let nordeaPassword = Provider(
+            id: "nordea-bankid",
+            displayName: "Nordea",
+            type: .bank,
+            status: .enabled,
+            credentialType: .password,
+            helpText: "",
+            isPopular: true,
+            fields: [],
+            groupDisplayName: "Nordea",
+            image: nil,
+            displayDescription: "Mobile BankID",
+            capabilities: .init(rawValue: 1266),
+            accessType: .other,
+            marketCode: "SE",
+            financialInstitutionID: "dde2463acf40501389de4fca5a3693a4",
+            financialInstitutionName: "Nordea"
+        )
+    }
 }
