11f0bf8395bb4c452280d0d246c6658319a6f1e0
Kasper Lahti <kasper.lahti@tink.se>
2019-10-10 14:10:17 +0200

    Move authorization error to separate file

--- Sources/TinkLink/Models/AuthorizationResponse.swift
+++ Sources/TinkLink/Models/AuthorizationResponse.swift
@@ -1,14 +1,3 @@
-import Foundation
-
 struct AuthorizationResponse: Decodable {
     let code: AuthorizationCode
-}
-
-struct AuthorizationError: Error, LocalizedError, Decodable {
-    let errorMessage: String
-    let errorCode: String
-
-    var errorDescription: String? {
-        return errorMessage
-    }
 }
