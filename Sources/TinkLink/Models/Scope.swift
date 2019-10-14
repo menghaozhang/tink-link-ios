d92bda7f4d0dd0b35d342943fe8edacf7d922546
Menghao Zhang <menghao.zhang@tink.se>
2019-10-14 14:07:17 +0200

    Moved the Access to nested struct with Scope

--- Sources/TinkLink/Models/Scope.swift
+++ Sources/TinkLink/Models/Scope.swift
@@ -1,12 +1,12 @@
 import Foundation
 public protocol ScopeType: CustomStringConvertible {
     static var name: String { get set }
-    var scope: TinkLink.Access { get set }
+    var access: TinkLink.Scope.Access { get set }
 }
 
 extension ScopeType {
     public var description: String {
-        return scope.descriptions.map { Self.name + ":" + $0 }.joined(separator: ",")
+        return access.descriptions.map { Self.name + ":" + $0 }.joined(separator: ",")
     }
 }
 
@@ -16,182 +16,182 @@ extension TinkLink {
         /// Access to all the user's account information, including balances.
         public struct Accounts: ScopeType {
             public static var name = "accounts"
-            public var scope: TinkLink.Access
-            public init(_ scope: TinkLink.Access) {
-                self.scope = scope
+            public var access: TinkLink.Scope.Access
+            public init(_ access: TinkLink.Scope.Access) {
+                self.access = access
             }
         }
 
         public struct Activities: ScopeType {
             public static var name = "activities"
-            public var scope: TinkLink.Access
-            public init(_ scope: TinkLink.Access) {
-                self.scope = scope
+            public var access: TinkLink.Scope.Access
+            public init(_ access: TinkLink.Scope.Access) {
+                self.access = access
             }
         }
 
         public struct Authorization: ScopeType {
             public static var name = "authorization"
-            public var scope: TinkLink.Access
-            public init(_ scope: TinkLink.Access) {
-                self.scope = scope
+            public var access: TinkLink.Scope.Access
+            public init(_ access: TinkLink.Scope.Access) {
+                self.access = access
             }
         }
 
         public struct Budgets: ScopeType {
             public static var name = "budgets"
-            public var scope: TinkLink.Access
-            public init(_ scope: TinkLink.Access) {
-                self.scope = scope
+            public var access: TinkLink.Scope.Access
+            public init(_ access: TinkLink.Scope.Access) {
+                self.access = access
             }
         }
 
         public struct Calendar: ScopeType {
             public static var name = "calendar"
-            public var scope: TinkLink.Access
-            public init(_ scope: TinkLink.Access) {
-                self.scope = scope
+            public var access: TinkLink.Scope.Access
+            public init(_ access: TinkLink.Scope.Access) {
+                self.access = access
             }
         }
 
         public struct Categories: ScopeType {
             public static var name = "categories"
-            public var scope: TinkLink.Access
-            public init(_ scope: TinkLink.Access) {
-                self.scope = scope
+            public var access: TinkLink.Scope.Access
+            public init(_ access: TinkLink.Scope.Access) {
+                self.access = access
             }
         }
 
         public struct Contacts: ScopeType {
             public static var name = "contacts"
-            public var scope: TinkLink.Access
-            public init(_ scope: TinkLink.Access) {
-                self.scope = scope
+            public var access: TinkLink.Scope.Access
+            public init(_ access: TinkLink.Scope.Access) {
+                self.access = access
             }
         }
 
         /// Access to the information describing the user's different bank credentials connected to Tink.
         public struct Credentials: ScopeType {
             public static var name = "credentials"
-            public var scope: TinkLink.Access
-            public init(_ scope: TinkLink.Access) {
-                self.scope = scope
+            public var access: TinkLink.Scope.Access
+            public init(_ access: TinkLink.Scope.Access) {
+                self.access = access
             }
         }
 
         public struct DataExports: ScopeType {
             public static var name = "data-exports"
-            public var scope: TinkLink.Access
-            public init(_ scope: TinkLink.Access) {
-                self.scope = scope
+            public var access: TinkLink.Scope.Access
+            public init(_ access: TinkLink.Scope.Access) {
+                self.access = access
             }
         }
 
         public struct Documents: ScopeType {
             public static var name = "documents"
-            public var scope: TinkLink.Access
-            public init(_ scope: TinkLink.Access) {
-                self.scope = scope
+            public var access: TinkLink.Scope.Access
+            public init(_ access: TinkLink.Scope.Access) {
+                self.access = access
             }
         }
 
         public struct Follow: ScopeType {
             public static var name = "follow"
-            public var scope: TinkLink.Access
-            public init(_ scope: TinkLink.Access) {
-                self.scope = scope
+            public var access: TinkLink.Scope.Access
+            public init(_ access: TinkLink.Scope.Access) {
+                self.access = access
             }
         }
 
         /// Access to the user's personal information that can be used for identification purposes.
         public struct Identity: ScopeType {
             public static var name = "identity"
-            public var scope: TinkLink.Access
-            public init(_ scope: TinkLink.Access) {
-                self.scope = scope
+            public var access: TinkLink.Scope.Access
+            public init(_ access: TinkLink.Scope.Access) {
+                self.access = access
             }
         }
 
         public struct Insights: ScopeType {
             public static var name = "insights"
-            public var scope: TinkLink.Access
-            public init(_ scope: TinkLink.Access) {
-                self.scope = scope
+            public var access: TinkLink.Scope.Access
+            public init(_ access: TinkLink.Scope.Access) {
+                self.access = access
             }
         }
 
         /// Access to the user's portfolios and underlying financial instruments.
         public struct Investments: ScopeType {
             public static var name = "investments"
-            public var scope: TinkLink.Access
-            public init(_ scope: TinkLink.Access) {
-                self.scope = scope
+            public var access: TinkLink.Scope.Access
+            public init(_ access: TinkLink.Scope.Access) {
+                self.access = access
             }
         }
 
         public struct Payment: ScopeType {
             public static var name = "payment"
-            public var scope: TinkLink.Access
-            public init(_ scope: TinkLink.Access) {
-                self.scope = scope
+            public var access: TinkLink.Scope.Access
+            public init(_ access: TinkLink.Scope.Access) {
+                self.access = access
             }
         }
 
         public struct Properties: ScopeType {
             public static var name = "properties"
-            public var scope: TinkLink.Access
-            public init(_ scope: TinkLink.Access) {
-                self.scope = scope
+            public var access: TinkLink.Scope.Access
+            public init(_ access: TinkLink.Scope.Access) {
+                self.access = access
             }
         }
 
         public struct Providers: ScopeType {
             public static var name = "providers"
-            public var scope: TinkLink.Access
-            public init(_ scope: TinkLink.Access) {
-                self.scope = scope
+            public var access: TinkLink.Scope.Access
+            public init(_ access: TinkLink.Scope.Access) {
+                self.access = access
             }
         }
 
         /// Access to all the user's statistics, which can include filters on statistic.type.
         public struct Statistics: ScopeType {
             public static var name = "statistics"
-            public var scope: TinkLink.Access
-            public init(_ scope: TinkLink.Access) {
-                self.scope = scope
+            public var access: TinkLink.Scope.Access
+            public init(_ access: TinkLink.Scope.Access) {
+                self.access = access
             }
         }
 
         public struct Suggestions: ScopeType {
             public static var name = "suggestions"
-            public var scope: TinkLink.Access
-            public init(_ scope: TinkLink.Access) {
-                self.scope = scope
+            public var access: TinkLink.Scope.Access
+            public init(_ access: TinkLink.Scope.Access) {
+                self.access = access
             }
         }
         /// Access to all the user's transactional data.
         public struct Transactions: ScopeType {
             public static var name = "transactions"
-            public var scope: TinkLink.Access
-            public init(_ scope: TinkLink.Access) {
-                self.scope = scope
+            public var access: TinkLink.Scope.Access
+            public init(_ access: TinkLink.Scope.Access) {
+                self.access = access
             }
         }
 
         public struct Transfer: ScopeType {
             public static var name = "transfer"
-            public var scope: TinkLink.Access
-            public init(_ scope: TinkLink.Access) {
-                self.scope = scope
+            public var access: TinkLink.Scope.Access
+            public init(_ access: TinkLink.Scope.Access) {
+                self.access = access
             }
         }
 
         /// Access to user profile data such as e-mail, date of birth, etc.
         public struct User: ScopeType {
             public static var name = "user"
-            public var scope: TinkLink.Access
-            public init(_ scope: TinkLink.Access) {
-                self.scope = scope
+            public var access: TinkLink.Scope.Access
+            public init(_ access: TinkLink.Scope.Access) {
+                self.access = access
             }
         }
 
--- Sources/TinkLink/Models/ScopeAccess.swift
+++ Sources/TinkLink/Models/ScopeAccess.swift
@@ -1,22 +1,22 @@
 import Foundation
 /// Access to user data is controlled by using OAuth2 security scopes or permissions. Each API customer is configured to have a set of scopes which control the maximum permitted data access.
-extension TinkLink {
+extension TinkLink.Scope {
     public struct Access: OptionSet, Hashable {
         public let rawValue: Int
         public init(rawValue: Int) {
             self.rawValue = rawValue
         }
 
-        public static let read = TinkLink.Access(rawValue: 1 << 0)
-        public static let write = TinkLink.Access(rawValue: 1 << 1)
-        public static let grant = TinkLink.Access(rawValue: 1 << 2)
-        public static let revoke = TinkLink.Access(rawValue: 1 << 3)
-        public static let refresh = TinkLink.Access(rawValue: 1 << 4)
-        public static let categorize = TinkLink.Access(rawValue: 1 << 5)
-        public static let execute = TinkLink.Access(rawValue: 1 << 6)
-        public static let create = TinkLink.Access(rawValue: 1 << 7)
-        public static let delete = TinkLink.Access(rawValue: 1 << 8)
-        public static let webHooks = TinkLink.Access(rawValue: 1 << 9)
+        public static let read = TinkLink.Scope.Access(rawValue: 1 << 0)
+        public static let write = TinkLink.Scope.Access(rawValue: 1 << 1)
+        public static let grant = TinkLink.Scope.Access(rawValue: 1 << 2)
+        public static let revoke = TinkLink.Scope.Access(rawValue: 1 << 3)
+        public static let refresh = TinkLink.Scope.Access(rawValue: 1 << 4)
+        public static let categorize = TinkLink.Scope.Access(rawValue: 1 << 5)
+        public static let execute = TinkLink.Scope.Access(rawValue: 1 << 6)
+        public static let create = TinkLink.Scope.Access(rawValue: 1 << 7)
+        public static let delete = TinkLink.Scope.Access(rawValue: 1 << 8)
+        public static let webHooks = TinkLink.Scope.Access(rawValue: 1 << 9)
 
         var descriptions: [String] {
             var descriptionStrings = [String]()
