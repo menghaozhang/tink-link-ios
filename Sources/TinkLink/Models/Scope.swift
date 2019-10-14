a1ccff252dccb7f0dfbc97f82878ed3a4a0063b5
Menghao Zhang <menghao.zhang@tink.se>
2019-10-14 12:07:08 +0200

    Moved the TinkLinkScope to a new file

--- Sources/TinkLink/Models/Scope.swift
+++ Sources/TinkLink/Models/Scope.swift
@@ -198,15 +198,3 @@ public struct UserScope: ScopeType {
         self.scope = scope
     }
 }
-
-/// Access to Tink is divided into scopes. The available scopes for Tink's APIs can be found in Tink console
-public struct TinkLinkScope: CustomStringConvertible {
-    public var scopes: [ScopeType]
-    public var description: String
-    public init(scopes: [ScopeType]) {
-        precondition(!scopes.isEmpty, "Tinklink scope is empty.")
-        self.scopes = scopes
-
-        description = scopes.map({ $0.description }).joined(separator: ",")
-    }
-}
