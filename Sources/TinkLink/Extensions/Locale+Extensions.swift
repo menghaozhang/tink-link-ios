e0880dfa087f7396c19ddce163ea91b9584a3f76
Lukas Lipka <lukaslipka@gmail.com>
2019-10-16 14:58:10 +0200

    Change casing.

--- Sources/TinkLink/Extensions/Locale+Extensions.swift
+++ Sources/TinkLink/Extensions/Locale+Extensions.swift
@@ -26,7 +26,7 @@ extension TinkLink {
     }
 
     /// Fallback locales if no other locale is available
-    static var fallBackLocale: Locale {
+    static var fallbackLocale: Locale {
         Locale(identifier: "en_US")
     }
 
@@ -37,7 +37,7 @@ extension TinkLink {
         } else if let regionCode = Locale.current.regionCode, let locale = availableLocaleWith(regionCode: regionCode) {
             return locale
         } else {
-            return fallBackLocale
+            return fallbackLocale
         }
     }
 }
