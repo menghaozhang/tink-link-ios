32a26772b89cba20a037f30af4b183bcd5be11df
Menghao Zhang <menghao.zhang@tink.se>
2019-10-17 10:17:58 +0200

    Updated the init

--- Examples/ProviderSelection/ProviderSelection/AppDelegate.swift
+++ Examples/ProviderSelection/ProviderSelection/AppDelegate.swift
@@ -6,6 +6,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        if let configuration = try? TinkLink.Configuration(processInfo: .processInfo) {
+            TinkLink.configure(with: configuration)
+        }
         window = UIWindow(frame: UIScreen.main.bounds)
         let providerListViewController = ProviderListViewController(style: .plain)
         let navigationController = UINavigationController(rootViewController: providerListViewController)
