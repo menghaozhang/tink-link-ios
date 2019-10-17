9e491a5fbf67d9218a88446cd1a178756a7e534d
Menghao Zhang <menghao.zhang@tink.se>
2019-10-17 10:52:51 +0200

    Revert back to have empty Dictionary as init value at Store

--- Sources/TinkLink/Stores/ProviderStore.swift
+++ Sources/TinkLink/Stores/ProviderStore.swift
@@ -7,7 +7,7 @@ final class ProviderStore {
     }
 
     private let tinkQueue = DispatchQueue(label: "com.tink.TinkLink.ProviderStore", attributes: .concurrent)
-    private var _providers: [Provider.ID: Provider]? {
+    private var _providers: [Provider.ID: Provider] = [:] {
         didSet {
             DispatchQueue.main.async {
                 NotificationCenter.default.post(name: .providerStoreChanged, object: self)
@@ -15,32 +15,23 @@ final class ProviderStore {
         }
     }
 
-    var providers: [Provider]? {
+    var providers: [Provider] {
         dispatchPrecondition(condition: .notOnQueue(tinkQueue))
-        if let providers = tinkQueue.sync(execute: { _providers }) {
-            return Array(providers.values)
-        }
-        return nil
+        let providers = tinkQueue.sync { _providers }
+        return Array(providers.values)
     }
 
-    subscript(market: Market) -> [Provider]? {
+    subscript(market: Market) -> [Provider] {
         dispatchPrecondition(condition: .notOnQueue(tinkQueue))
-        // TODO: This will not work when having multi market, need to store provider by market
-        if let providers = tinkQueue.sync(execute: { _providers?.values.filter({ $0.marketCode == market.code }) }) {
-            return providers
-        }
-        return nil
+        let providers = tinkQueue.sync { _providers.values.filter({ $0.marketCode == market.code }) }
+        return providers
     }
 
     func store(_ providers: [Provider]) {
         tinkQueue.async(qos: .default, flags: .barrier) {
             let newProviders = Dictionary(grouping: providers, by: { $0.id })
                 .compactMapValues { $0.first }
-            if self._providers == nil {
-                self._providers = newProviders
-            } else {
-            self._providers?.merge(newProviders, uniquingKeysWith: { (_, new) in new })
-            }
+            self._providers.merge(newProviders, uniquingKeysWith: { (_, new) in new })
         }
     }
 }
