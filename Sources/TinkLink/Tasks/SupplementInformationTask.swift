adfaf23a224806034bcaea3576048138ab16b58f
Kasper Lahti <kasper.lahti@tink.se>
2019-10-17 16:20:06 +0200

    Add docs for supplemental information task

--- Sources/TinkLink/Tasks/SupplementInformationTask.swift
+++ Sources/TinkLink/Tasks/SupplementInformationTask.swift
@@ -1,6 +1,13 @@
+/// A task that handles submitting supplemental information for a credential.
+///
+/// This task is usually given when an AddCredentialTask's status changes to `awaitingSupplementalInformation`.
+/// Use this task to submit supplmental information for the credential.
+/// If the user dismiss supplementing information, by e.g. closing the form, you need to call `cancel()` to stop adding the credential.
 public class SupplementInformationTask {
     private let credentialService: CredentialService
     private var callRetryCancellable: RetryCancellable?
+
+    /// The credential that's awaiting supplemental information.
     public private(set) var credential: Credential
 
     private let completionHandler: (Result<Void, Error>) -> Void
@@ -11,6 +18,9 @@ public class SupplementInformationTask {
         self.completionHandler = completionHandler
     }
 
+    /// Submits the provided form fields.
+    ///
+    /// - Parameter form: This is a form with fields from the credential that had status `awaitingSupplementalInformation`.
     public func submit(_ form: Form) {
         callRetryCancellable = credentialService.supplementInformation(credentialID: credential.id, fields: form.makeFields(), completion: { [weak self] result in
             self?.completionHandler(result)
@@ -18,6 +28,9 @@ public class SupplementInformationTask {
         })
     }
 
+    /// Tells the credential to stop waiting for supplemental information.
+    ///
+    /// Call this method if the user dismiss the form to supplement information.
     public func cancel() {
         callRetryCancellable = credentialService.cancelSupplementInformation(credentialID: credential.id, completion: { [weak self] result in
             self?.completionHandler(result)
