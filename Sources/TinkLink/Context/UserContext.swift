5f3d3d536c9b11a9972a28b6cb6046ec85344ba5
Kasper Lahti <kasper.lahti@tink.se>
2019-10-23 08:05:54 +0200

    Update documentation comments

--- Sources/TinkLink/Context/UserContext.swift
+++ Sources/TinkLink/Context/UserContext.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-/// An object that you use to register for an access token that will be used in other TinkLink APIs.
+/// An object that you use to create a user that will be used in other TinkLink APIs.
 public final class UserContext {
     private var userService: UserService
     private var retryCancellable: RetryCancellable?
@@ -12,12 +12,12 @@ public final class UserContext {
         self.userService = UserService(tinkLink: tinkLink)
     }
 
-    /// Register for an access token for a specific market and locale.
-    ///  - Note: If an access token has registered by this `UserContext` then completion will be triggered immediately.
+    /// Create a user for a specific market and locale.
+    ///  - Note: If a user has been created by this `UserContext` already then the completion will be triggered immediately.
     ///
-    /// - Parameter market: Register a `Market` for authentication,  will use the default market if nothing is provided.
-    /// - Parameter locale: Register a `Locale` for authentication,  will use the default locale in TinkLink if nothing is provided.
-    public func authenticateIfNeeded(for market: Market = .defaultMarket, locale: Locale = TinkLink.defaultLocale, completion: @escaping (Result<User, Error>) -> Void) -> RetryCancellable? {
+    /// - Parameter market: Register a `Market` for creating the user, will use the default market if nothing is provided.
+    /// - Parameter locale: Register a `Locale` for creating the user, will use the default locale in TinkLink if nothing is provided.
+    public func createUserIfNeeded(for market: Market = .defaultMarket, locale: Locale = TinkLink.defaultLocale, completion: @escaping (Result<User, Error>) -> Void) -> RetryCancellable? {
         if let user = user {
             completion(.success(user))
         } else if retryCancellable == nil {
