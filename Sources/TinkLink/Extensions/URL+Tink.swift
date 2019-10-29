70bb46877191b102657955fd50bfcef5dca01805
Menghao Zhang <menghao.zhang@tink.se>
2019-10-29 16:50:28 +0100

    Safer check

--- Sources/TinkLink/Extensions/URL+Tink.swift
+++ Sources/TinkLink/Extensions/URL+Tink.swift
@@ -2,7 +2,7 @@ import Foundation
 
 extension URL {
     var tinLinkAppURI: URL {
-        if let scheme = scheme, !scheme.contains("http"), absoluteString.hasPrefix("\(scheme)://"), !absoluteString.hasPrefix("\(scheme):///") {
+        if let scheme = scheme, !scheme.hasPrefix("http"), absoluteString.hasPrefix("\(scheme)://"), !absoluteString.hasPrefix("\(scheme):///") {
             let appUri = absoluteString.replacingOccurrences(of: "\(scheme)://", with: "\(scheme):///")
             guard let appURI = URL(string: appUri) else { fatalError("Invalid URL") }
             return appURI
