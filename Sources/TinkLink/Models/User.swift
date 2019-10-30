d951c7b8e5f1d413064d541026dd59f2282d7800
Menghao Zhang <menghao.zhang@tink.se>
2019-10-30 14:54:14 +0100

    Removed the market and locale from the TinkLink

--- Sources/TinkLink/Models/User.swift
+++ Sources/TinkLink/Models/User.swift
@@ -1,4 +1,14 @@
+import Foundation
+
 /// A user in the Tink API.
 public struct User {
     let accessToken: AccessToken
+
+    /// The market to use.
+    ///
+    /// This is used by TinkLink when creating an anonymous user and when fetching providers.
+    let market: Market
+
+    /// The locale to use.
+    let locale: Locale
 }
