fbcf10db7dce361f54b7728f343a9716ce9ac826
Menghao Zhang <menghao.zhang@tink.se>
2019-10-31 10:24:26 +0100

    Updated the test

--- Tests/TinkLinkTests/ConfigurationTests.swift
+++ Tests/TinkLinkTests/ConfigurationTests.swift
@@ -10,21 +10,11 @@ class ConfigurationTests: XCTestCase {
         XCTAssertNotNil(link.configuration)
     }
 
-    func testMarketConfiguration() {
-        let redirectURI = URL(string: "http://my-customer-app.com/authentication")!
-        let configuration = TinkLink.Configuration(clientID: "abc", redirectURI: redirectURI, market: "SE")
-        let link = TinkLink(configuration: configuration)
-        XCTAssertEqual(link.configuration.market.rawValue, "SE")
-        XCTAssertEqual(link.client.market.rawValue, "SE")
-    }
-
     func testConfigureSharedTinkLinkWithConfiguration() {
         TinkLink._shared = nil
         let redirectURI = URL(string: "my-customer-app://authentication")!
-        let configuration = TinkLink.Configuration(clientID: "abc", redirectURI: redirectURI, market: "SE")
+        let configuration = TinkLink.Configuration(clientID: "abc", redirectURI: redirectURI)
         TinkLink.configure(with: configuration)
-        XCTAssertEqual(TinkLink.shared.configuration.market.rawValue, "SE")
-        XCTAssertEqual(TinkLink.shared.client.market.rawValue, "SE")
         XCTAssertEqual(TinkLink.shared.configuration.redirectURI, redirectURI)
     }
 }
