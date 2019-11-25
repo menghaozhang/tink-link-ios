897e86281ffe3691851c6ca8af640a3de3f199ce
Menghao Zhang <menghao.zhang@tink.se>
2019-11-25 16:41:35 +0100

    Removed the unnecessary market and locale

--- Sources/TinkLink/Models/User.swift
+++ Sources/TinkLink/Models/User.swift
@@ -3,20 +3,10 @@ import Foundation
 /// A user in the Tink API.
 public struct User {
     let accessToken: AccessToken
-
-    /// The market with which the user was created.
-    ///
-    /// This is used by TinkLink when creating an anonymous user and when fetching providers.
-    public let market: Market
-
-    /// The locale with which the user was created.
-    public let locale: Locale
 }
 
 extension User {
-    init(accessToken: String, market: Market, locale: Locale) {
+    init(accessToken: String) {
         self.accessToken = AccessToken(accessToken)
-        self.market = market
-        self.locale = locale
     }
 }
